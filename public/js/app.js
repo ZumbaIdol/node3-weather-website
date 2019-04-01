376044abdc6847bdd0c20d3d897ca64e88884ef8
David A. Foote <david.a.foote@gmail.com>
2019-03-31 19:18:57 -0500

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,3 @@
-// fetch('http://localhost:3000/weather?address=!').then((response) => {
-//   response.json().then((data) => {
-//     if (data.error) {
-//       console.log(data.error)
-//     } else {
-//       console.log(data.location)
-//       console.log(data.forecast)
-//     }
-//   })
-// })
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
@@ -22,7 +11,7 @@ weatherForm.addEventListener('submit', (e) => {
   messageOne.textContent = 'Intentando obtener el pronóstico...'
   messageTwo.textContent = ''
   
-  fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+  fetch('/weather?address=' + location).then((response) => {
     response.json().then((data) => {
       if (data.error) {
         messageOne.textContent = data.error
